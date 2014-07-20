c52626b09f6f6eb6b6fcd616d930bcdb0f8ad53d
Sam Wilson <sam@samwilson.id.au>
2014-07-20 18:47:24 +0800

    Switch to using page properties for storage.

--- Person.php
+++ Person.php
@@ -2,6 +2,7 @@
 
 class GenealogyPerson {
 
+	/** @var Title */
 	private $title;
 
 	private $parents;
@@ -31,7 +32,7 @@ class GenealogyPerson {
 	 * @return boolean
 	 */
 	public function hasDates() {
-		return $this->getBirthDate()!==FALSE;
+		return $this->getBirthDate() !== FALSE;
 	}
 
 	/**
@@ -41,16 +42,12 @@ class GenealogyPerson {
 	 * @return string
 	 */
 	public function getBirthDate() {
-		$pattern = '/{{\#'.$this->magicRegex.':\s*person.*birth date=([^|}]*)/';
-		preg_match_all($pattern, $this->getText(), $matches);
-		if (!isset($matches[1][0])) {
-			return FALSE;
-		}
-		$time = strtotime($matches[1][0]);
-		if ($time!==FALSE) {
+		$birthDate = $this->getPropSingle('birth date');
+		$time = strtotime($birthDate);
+		if ($time !== FALSE) {
 			return date('j F Y', $time);
 		} else {
-			return $matches[1][0];
+			return $birthDate;
 		}
 	}
 
@@ -63,15 +60,7 @@ class GenealogyPerson {
 		if (is_array($this->parents)) {
 			return $this->parents;
 		}
-		$this->parents = array();
-		$pattern = '/{{\#'.$this->magicRegex.':\s*parent\s*\|\s*([^|}]*)/';
-		preg_match_all($pattern, $this->getText(), $matches);
-		if (isset($matches[1])) {
-			foreach ($matches[1] as $match) {
-				$parentTitle = Title::newFromText($match);
-				$this->parents[$parentTitle->getPrefixedDBkey()] = new GenealogyPerson($parentTitle);
-			}
-		}
+		$this->parents = $this->getPropMulti('parent');
 		return $this->parents;
 	}
 
@@ -102,7 +91,11 @@ class GenealogyPerson {
 		if (is_array($this->partners)) {
 			return $this->partners;
 		}
-		$this->partners = $this->whatLinksHere('partner');
+		$this->partners = array_merge(
+			$this->getPropInbound('partner'),
+			$this->getPropMulti('partner')
+		);
+		//unset($this->partners[$this->title->getPrefixedDBkey()]);
 		return $this->partners;
 	}
 
@@ -115,75 +108,145 @@ class GenealogyPerson {
 		if (is_array($this->children)) {
 			return $this->children;
 		}
-		$this->children = array();
-		$prefexedTitle = $this->title->getPrefixedDBkey();
-		$dbr = wfGetDB(DB_SLAVE);
-		$res = $dbr->select(
-			array('pl' => 'pagelinks', 'p' => 'page'),
-			array('page_namespace', 'page_title'), // columns
-			array(// conditions
-				'pl_title' => $prefexedTitle,
-				'pl_from = page_id',
-				'pl_namespace = page_namespace'
-			),
-			__METHOD__,
-			array(),
-			array('page' => array())
-		);
-		foreach ($res as $row) {
-			$childTitle = Title::makeTitle($row->page_namespace, $row->page_title);
-			$poss_child = new WikiPage($childTitle);
-			$content = $poss_child->getContent();
-			$text = ContentHandler::getContentText($content);
-			$pattern = '/{{\#'.$this->magicRegex.':\s*parent\s*\|\s*'.$prefexedTitle.'/';
-			if(preg_match($pattern, $text)===1) {
-				$this->children[] = new GenealogyPerson($childTitle);
-			}
-		}
+		$this->children = $this->getPropInbound('parent');
+//		$this->children = array();
+//		$dbr = wfGetDB(DB_SLAVE);
+//		$children = $dbr->select(
+//			array('pp'=>'page_props', 'p'=>'page'), // tables
+//			array('pp_value', 'page_title'), // columns
+//			array( // where conditions
+//				'pp_value' => $this->title->getPrefixedText(),
+//				"pp_propname LIKE 'genealogy parent %'",
+//				'pp_page = page_id',
+//			),
+//			__METHOD__,
+//			array(),
+//			array('page'=>array())
+//		);
+//		foreach ($children as $child) {
+//			$childTitle = Title::newFromText($child->page_title);
+//			$this->children[$childTitle->getPrefixedDBkey()] = new GenealogyPerson($childTitle);
+//		}
+
+//		$prefexedTitle = $this->title->getPrefixedDBkey();
+//		$dbr = wfGetDB(DB_SLAVE);
+//		$res = $dbr->select(
+//			array('pl' => 'pagelinks', 'p' => 'page'),
+//			array('page_namespace', 'page_title'), // columns
+//			array(// conditions
+//				'pl_title' => $prefexedTitle,
+//				'pl_from = page_id',
+//				'pl_namespace = page_namespace'
+//			),
+//			__METHOD__,
+//			array(),
+//			array('page' => array())
+//		);
+//		foreach ($res as $row) {
+//			$childTitle = Title::makeTitle($row->page_namespace, $row->page_title);
+//			$poss_child = new WikiPage($childTitle);
+//			$content = $poss_child->getContent();
+//			$text = ContentHandler::getContentText($content);
+//			$pattern = '/{{\#'.$this->magicRegex.':\s*parent\s*\|\s*'.$prefexedTitle.'/';
+//			if(preg_match($pattern, $text)===1) {
+//				$this->children[] = new GenealogyPerson($childTitle);
+//			}
+//		}
 		return $this->children;
 	}
 
-	/**
-	 * Get an array of GenealogyPerson objects built from pages that link to this one with the
-	 * relationship of $as.
-	 * @param string $as The relationship type, either 'parent' or 'partner'.
-	 */
-	private function whatLinksHere($as) {
+	private function getPropInbound($type) {
 		$out = array();
-		$prefexedTitle = $this->title->getPrefixedDBkey();
 		$dbr = wfGetDB(DB_SLAVE);
-		$res = $dbr->select(
-			array('pl' => 'pagelinks', 'p' => 'page'),
-			array('page_namespace', 'page_title'), // columns
-			array(// conditions
-				'pl_title' => $prefexedTitle,
-				'pl_from = page_id',
-				'pl_namespace = page_namespace'
+		$results = $dbr->select(
+			array('pp'=>'page_props', 'p'=>'page'), // tables
+			array('pp_value', 'page_title'), // columns
+			array( // where conditions
+				'pp_value' => $this->title->getPrefixedText(),
+				"pp_propname LIKE 'genealogy $type %'",
+				'pp_page = page_id',
 			),
 			__METHOD__,
 			array(),
-			array('page' => array())
+			array('page'=>array())
 		);
-		foreach ($res as $row) {
-			$linkTitle = Title::makeTitle($row->page_namespace, $row->page_title);
-			$possibleLink = new WikiPage($linkTitle);
-			$text = ContentHandler::getContentText($possibleLink->getContent());
-			$pattern = '/{{\#'.$this->magicRegex.':\s*'.$as.'\s*\|\s*'.$prefexedTitle.'/';
-			if(preg_match($pattern, $text)===1) {
-				$out[$linkTitle->getPrefixedDBkey()] = new GenealogyPerson($linkTitle);
-			}
+		foreach ($results as $res) {
+			$title = Title::newFromText($res->page_title);
+			$out[$title->getPrefixedDBkey()] = new GenealogyPerson($title);
 		}
 		return $out;
 	}
 
-	/**
-	 * Get the text of this page.
-	 * @return string
-	 */
-	private function getText() {
-		$selfPage = new WikiPage($this->title);
-		$text = ContentHandler::getContentText($selfPage->getContent());
-		return $text;
+	public function getPropSingle($prop) {
+		$dbr = wfGetDB(DB_SLAVE);
+		return $dbr->selectField(
+			'page_props', // table to use
+			'pp_value', // Field to select
+			array( 'pp_page' => $this->title->getArticleID(), 'pp_propname' => "genealogy $prop" ), // where conditions
+			__METHOD__
+		);
 	}
+
+	private function getPropMulti($type) {
+		$out = array();
+		$dbr = wfGetDB(DB_SLAVE);
+		$results = $dbr->select(
+			'page_props', // table to use
+			'pp_value', // Field to select
+			array( // where conditions
+				'pp_page' => $this->title->getArticleID(),
+				"pp_propname LIKE 'genealogy $type %'"
+			),
+			__METHOD__
+		);
+		foreach ($results as $result) {
+			$title = Title::newFromText($result->pp_value);
+			$out[$title->getPrefixedDBkey()] = new GenealogyPerson($title);
+		}
+		return $out;
+	}
+
+//	/**
+//	 * Get an array of GenealogyPerson objects built from pages that link to this one with the
+//	 * relationship of $as.
+//	 * @param string $as The relationship type, either 'parent' or 'partner'.
+//	 */
+//	private function whatLinksHere($as) {
+//		$out = array();
+//		$prefexedTitle = $this->title->getPrefixedDBkey();
+//		$dbr = wfGetDB(DB_SLAVE);
+//		$res = $dbr->select(
+//			array('pl' => 'pagelinks', 'p' => 'page'),
+//			array('page_namespace', 'page_title'), // columns
+//			array(// conditions
+//				'pl_title' => $prefexedTitle,
+//				'pl_from = page_id',
+//				'pl_namespace = page_namespace'
+//			),
+//			__METHOD__,
+//			array(),
+//			array('page' => array())
+//		);
+//		foreach ($res as $row) {
+//			$linkTitle = Title::makeTitle($row->page_namespace, $row->page_title);
+//			$possibleLink = new WikiPage($linkTitle);
+//			$text = ContentHandler::getContentText($possibleLink->getContent());
+//			$pattern = '/{{\#'.$this->magicRegex.':\s*'.$as.'\s*\|\s*'.$prefexedTitle.'/';
+//			if(preg_match($pattern, $text)===1) {
+//				$out[$linkTitle->getPrefixedDBkey()] = new GenealogyPerson($linkTitle);
+//			}
+//		}
+//		return $out;
+//	}
+//
+//	/**
+//	 * Get the text of this page.
+//	 * @return string
+//	 */
+//	private function getText() {
+//		$selfPage = new WikiPage($this->title);
+//		$text = ContentHandler::getContentText($selfPage->getContent());
+//		return $text;
+//	}
 
 }
